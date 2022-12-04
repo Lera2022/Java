4a2c170684cf62c5a38181f35cc5948ce9b54346
Lera <valeriiakolesnikova21@gmail.com>
2022-12-04 14:36:48 +0300

    first commit

---
+++ java_projects/program.java
@@ -0,0 +1,46 @@
+package java_projects;
+
+/**
+ * program
+ */
+public class program {
+    public static void main(String[] args) {
+        // String s = null;
+        // System.out.println(s);
+        // float f = 123.45f;
+        // char ch = '{';
+        char ch = 123;
+        System.out.println(ch);
+    }
+    
+}
+
+class Program 
+{
+    public static void main (String[] args) {
+        short age = 10;
+        int salary = 123456;
+        System.out.println(age);        //10
+        System.out.println(salary);     //123456
+    }
+}
+
+class Program2 
+{
+    public static void main (String[] args) {
+        float e = 2.7f;
+        double pi = 3.1415;
+        System.out.println(e);        //2.7
+        System.out.println(pi);     //3.1415
+    }
+}
+
+class Program3 
+{
+    public static void main (String[] args) {
+        char ch = '1';
+        System.out.println(Character.isDigit(ch));        //true
+        ch = 'a';
+        System.out.println(Character.isDigit(ch));        //false
+    }
+}
