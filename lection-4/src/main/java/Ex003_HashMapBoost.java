3e8c466f8a78b7d2eb955a99486a25a48ece516d
Lera <valeriiakolesnikova21@gmail.com>
2022-12-17 17:22:52 +0300

    lection 5 added

---
+++ lection-4/src/main/java/Ex003_HashMapBoost.java
@@ -0,0 +1,19 @@
+//Как ускорить работу
+
+import java.util.*;
+
+
+
+public class Ex003_HashMapBoost {
+
+    public static void main(String[] args) {
+
+        Map<Integer,String> map1 = new HashMap<>();
+
+        Map<Integer,String> map2 = new HashMap<>(9);
+
+        Map<Integer,String> map3 = new HashMap<>(9, 1.0f);
+
+    }
+
+}
